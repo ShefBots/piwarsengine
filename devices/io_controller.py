03c3de16f8a78fedfc878c233b93494a0f915f2c
Fred <guru.three@gmail.com>
2024-04-06 15:33:18 +0100

    add gripper controls

--- devices/io_controller.py
+++ devices/io_controller.py
@@ -12,6 +12,13 @@ COM_READ_TOF_RECV = make_command('T', SSHORT)
 
 COM_SET_LED_SEND = make_command('L', UBYTE * 4)
 
+COM_SET_GRIPPER_SEND = make_command('G', UBYTE)
+COM_READ_GRIPPER_SEND = make_command('g')
+COM_READ_GRIPPER_RECV = make_command('g', UBYTE)
+
+GRIPPER_CLOSED = 0
+GRIPPER_OPEN = 1
+GRIPPER_UNKNOWN = 2
 
 class IOController:
     NAME = "IO Controller"
@@ -40,6 +47,29 @@ class IOController:
         except ValueError as e:
             print(e)
             return -99
+
+    def open_gripper(self):
+        # could also check here if it was in the process of opening so
+        # as not to send open twice
+        if self.gripper_state() == GRIPPER_OPEN:
+            return
+        self.__comms.send(COM_SET_GRIPPER_SEND, GRIPPER_OPEN)
+
+    def close_gripper(self):
+        # same again but could check if it was already closing
+        if self.gripper_state() == GRIPPER_CLOSED:
+            return
+        self.__comms.send(COM_SET_GRIPPER_SEND, GRIPPER_CLOSED)
+
+    def gripper_state(self):
+        self.__comms.send(COM_READ_GRIPPER_SEND)
+
+        # Catch an infrequent checksum error that occurs
+        try:
+            return self.__comms.receive(COM_READ_GRIPPER_RECV)
+        except ValueError as e:
+            print(e)
+            return GRIPPER_UNKNOWN
 
     def set_led(self, led, r, g, b):
         self.__comms.send(COM_SET_LED_SEND, led, r, g, b)
